1b523e0b68d12036d4a0748e58050ab1931f327d
Jason Reid <jason.reid@r3labs.io>
2016-12-01 17:03:48 +0000

    test

--- manifests/site.pp
+++ manifests/site.pp
@@ -12,16 +12,6 @@ node default {
   # write hostname to index.html
   exec { 'update_index':
     command => "/bin/hostname | tee /var/www/html/index.html",
-    command => "apt install -y php libapache2-mod-php php-mcrypt php-mysql",
-#    command => "systemctl restart apache2",
-#    command => "groupadd www",
-#    command => "usermod -a -G www ubuntu",
-#    command => "chown -R root:www /var/www",
-#    command => "chmod 2775 /var/www",
-#    command => "find /var/www -type d -exec sudo chmod 2775 {} +",
-#    command => "find /var/www -type f -exec sudo chmod 0664 {} +",
-#    command => "cd /var/www",
-#    command => "mkdir inc",
   }
 
 }
