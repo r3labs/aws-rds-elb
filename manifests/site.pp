1f06b2b60b394eac29d8cccc63f180a4549b7b59
Jason Reid <jason.reid@r3labs.io>
2016-12-01 17:27:25 +0000

    test

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,22 +2,18 @@ node default {
 
   include cron-puppet
 
-
   $build_package = [ 'apache2', 'php', 'libapache2-mod-php', 'php-mcrypt', 'php-mysql' ]
-
   package {$build_package:
     ensure => installed,
   }
 
-#  # write hostname to index.html
-#  exec { 'update_index':
-#    command => "/bin/hostname | tee /var/www/html/index.html",
-#  }
+  # write hostname to index.html
+  exec { 'update_index':
+    command => "/bin/hostname | tee /var/www/html/index.html",
+  }
 
   $bars = [ "/bin/hostname | tee /var/www/html/index.html", "mkdir /var/www/inc" ]
-  Foo { $bars : }
-  exec { "echo Done" :
-      require => [ Foo[$bars] ]
+  exec { $bars :
   }
 
 }
