b028336a54a82d4fe48d137ac4d74dd9a5eb4464
Jason Reid <jason.reid@r3labs.io>
2016-12-01 15:08:32 +0000

    test5

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,15 +3,20 @@ node default {
   include cron-puppet
 
   # install apache2 package
-  package { 'apache2':
-    ensure => installed,
-    notify => Exec['update_index']
+#  package { 'apache2':
+#    ensure => installed,
+#    notify => Exec['update_index']
+#  }
+
+  class packages {
+    $package_list = [ 'apache2', 'php', 'libapache2-mod-php', 'php-mcrypt', 'php-mysql' ]
+    package { $package_list: ensure => 'installed' }
   }
 
   # write hostname to index.html
   exec { 'update_index':
-#    command => "/bin/hostname | tee /var/www/html/index.html",
-    command => "apt install -y php libapache2-mod-php php-mcrypt php-mysql",
+    command => "/bin/hostname | tee /var/www/html/index.html",
+#    command => "apt install -y php libapache2-mod-php php-mcrypt php-mysql",
 #    command => "systemctl restart apache2",
 #    command => "groupadd www",
 #    command => "usermod -a -G www ubuntu",
