014aba10ad61c5ac12549f2e9ec8800d9b217d2e
Jason Reid <jason.reid@r3labs.io>
2016-12-01 18:02:32 +0000

    test

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,12 +3,22 @@ node default {
   include cron-puppet
 
   $build_package = [ 'apache2', 'php', 'libapache2-mod-php', 'php-mcrypt', 'php-mysql' ]
+
   package {$build_package:
     ensure => installed,
-    notify => Exec[$cmds]
   }
 
-  $cmds = [ "/bin/hostname | tee /var/www/html/index.html", "/bin/mkdir /var/www/inc" ]
+  $cmds = [
+    "/bin/hostname | tee /var/www/html/index.html",
+    "/usr/sbin/groupadd www",
+    "/usr/sbin/usermod -a -G www ubuntu",
+    "/bin/chown -R root:www /var/www"
+    "/bin/chmod 2775 /var/www",
+    "/usr/bin/find /var/www -type d -exec sudo chmod 2775 {} +",
+    "/usr/bin/find /var/www -type f -exec sudo chmod 0664 {} +",
+    "/bin/mkdir /var/www/inc",
+  ]
+
   exec { $cmds :
   }
 
