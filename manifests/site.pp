780a19be2a0510c484d32deb7dd317ef4e6b6304
Jason Reid <jason.reid@r3labs.io>
2016-12-01 17:12:07 +0000

    test

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,9 +9,15 @@ node default {
     ensure => installed,
   }
 
-  # write hostname to index.html
-  exec { 'update_index':
-    command => "/bin/hostname | tee /var/www/html/index.html",
+#  # write hostname to index.html
+#  exec { 'update_index':
+#    command => "/bin/hostname | tee /var/www/html/index.html",
+#  }
+
+  $bars = [ "/bin/hostname | tee /var/www/html/index.html", "mkdir /var/www/inc" ]
+  foo { $bars : }
+  exec { "echo Done" :
+      require => [ Foo[$bars] ]
   }
 
 }
