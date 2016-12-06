0726de07c0a15368c70d1ea740a52fca7de53249
Jason Reid <jason.reid@r3labs.io>
2016-12-06 10:56:25 +0000

    test

--- modules/cron_puppet/manifests/init.pp
+++ modules/cron_puppet/manifests/init.pp
@@ -1,17 +1,17 @@
 class cron_puppet {
     file { 'post-hook':
-        ensure  => file,
-        path    => '/etc/puppet/.git/hooks/post-merge',
-        source  => 'puppet:///modules/cron-puppet/post-merge',
-        mode    => 0755,
-        owner   => root,
-        group   => root,
+        ensure => file,
+        path => '/etc/puppet/.git/hooks/post-merge',
+        source => 'puppet:///modules/cron-puppet/post-merge',
+        mode => 0755,
+        owner => root,
+        group => root,
     }
     cron { 'puppet-apply':
-        ensure  => present,
+        ensure => present,
         command => 'cd /etc/puppet ; /usr/bin/git pull',
-        user    => root,
-        minute  => '*/30',
+        user => root,
+        minute => '*/30',
         require => File['post-hook'],
     }
 }
